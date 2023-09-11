c0368c880d131ffa3b025a0589c2570e463437c5
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-09-11 17:43:46 +0530

    import java.util.*; public class greate{     public static void main (String args[])     {         int n,sum=0,avg,count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();             sum=sum+a[i];         }            avg=sum/n;            for(int i=0;i<n;i++){                if(a[i]>=avg){                   count++;                }            }
            System.out.println(count);
        
        }
    }

---
+++ Elements_greater_than_average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class greate{
+    public static void main (String args[])
+    {
+        int n,sum=0,avg,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            sum=sum+a[i];
+        }
+           avg=sum/n;
+           for(int i=0;i<n;i++){
+               if(a[i]>=avg){
+                  count++;
+               }
+           }
+        
+        System.out.println(count);
+    
+    }
+}
